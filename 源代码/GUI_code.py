1f0786402a79024a2d8c9b9978f3145e17674632
arnomiller <l498191503@163.com>
2023-01-04 22:51:22 +0800

    V0.3.4版本（2023.01.04） 修复部分恶性bug，重新打包软件

--- 源代码/GUI_code.py
+++ 源代码/GUI_code.py
@@ -1,42 +1,65 @@
 import os
+import shutil
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import *
 
 win = tk.Tk()
 win.title("无人机重命名软件V0.3.1 Code by LYC")
-win.geometry('560x400+500+200')
+win.geometry('770x200+500+200')
 win.iconphoto(False, tk.PhotoImage(file='UAV.png'))
 frame = tk.Frame(win)
 # 杆塔号输入控件
 label_1 = tk.Label(win, text="1.请输入杆塔号")
 label_1.grid(row=0, column=0)
 text_v = tk.StringVar()
-entry_1 = tk.Entry(win, textvariable=text_v)
+entry_1 = tk.Entry(win, textvariable=text_v, width=30)
 entry_1.grid(row=1, column=0)
 
+
 # 下拉菜单1.命名模板
+def show_num_of_model(event):
+    model_cwd = os.getcwd() + "\\无人机精飞照片命名\\" + Select_com_001.get()
+    with open(model_cwd, encoding="utf-8") as f:
+        new_name = f.read().splitlines()
+    varLabel_1.set(len(new_name))
+
+
 model_dir_list = os.listdir('无人机精飞照片命名')
 model_dir_list.sort(key=lambda x: int(x.split('.')[0]))
-xVariable = tk.StringVar()
+xVariable_2 = tk.StringVar()
 label_2 = tk.Label(win, text="2.请选择命名模板")
 label_2.grid(row=0, column=1)
-Select_com_001 = tk.ttk.Combobox(win, textvariable=xVariable, state='readonly', values=model_dir_list)
+Select_com_001 = tk.ttk.Combobox(win, textvariable=xVariable_2, values=model_dir_list, width=30)
+Select_com_001.bind('<<ComboboxSelected>>', show_num_of_model)
 Select_com_001.grid(row=1, column=1)
+varLabel_1 = tk.StringVar()
+label_5 = tk.Label(win, textvariable=varLabel_1)
+label_5.grid(row=2, column=1)
+
 
 # 下拉菜单2.选择需要处理的文件夹
+def show_num_of_photo(event):
+    work_cwd = os.getcwd() + '\\未处理照片\\' + Select_com_002.get()
+    photo_dir_list = os.listdir(work_cwd)
+    varLabel_2.set(len(photo_dir_list))
+
+
 file_dir_list = os.listdir('未处理照片')
-file_dir_list.sort(key=lambda x: int(x.split('.')[0]))
-xVariable = tk.StringVar()
+xVariable_3 = tk.StringVar()
 label_3 = tk.Label(win, text="3.请选择需要命名的杆塔照片")
 label_3.grid(row=0, column=2)
-Select_com_002 = tk.ttk.Combobox(win, textvariable=xVariable, state='readonly', values=file_dir_list)
+Select_com_002 = tk.ttk.Combobox(win, textvariable=xVariable_3, state='readonly', values=file_dir_list, width=30)
+Select_com_002.bind('<<ComboboxSelected>>', show_num_of_photo)
 Select_com_002.grid(row=1, column=2)
+varLabel_2 = tk.StringVar()
+label_6 = tk.Label(win, textvariable=varLabel_2)
+label_6.grid(row=2, column=2)
 
 
 # 开始按钮功能，重命名函数
 def pic_Rename():
-    work_cwd = os.getcwd() + '\\未处理照片\\'
+    work_cwd = os.getcwd() + '\\未处理照片\\' + Select_com_002.get()
     model_cwd = os.getcwd() + "\\无人机精飞照片命名\\" + Select_com_001.get()
     num_of_tower = entry_1.get()
 
@@ -57,21 +80,23 @@ def pic_Rename():
             showinfo('提示', '照片数量等于模板数量，开始转换')
             os.makedirs(os.path.abspath('已处理照片') + "\\" + num_of_tower)
             for i in range(len(photo_dir_list)):
-                os.rename(work_cwd + photo_dir_list[i],
+                os.rename(work_cwd + '\\' + photo_dir_list[i],
                           os.path.abspath("已处理照片" + "\\" + num_of_tower) + '\\' + num_of_tower + new_name[
                               i] + ".jpg")
             print("转换完成。")
             showinfo('提示', '转换完成')
+            shutil.rmtree(work_cwd)
         elif len(photo_dir_list) > len(new_name):
             print("照片数量多于模板数量，仅修改模板数量照片")
             showinfo('提示', '照片数量多于模板数量，仅修改模板数量照片')
             os.makedirs(os.path.abspath('已处理照片') + "\\" + num_of_tower)
             for i in range(len(new_name)):
-                os.rename(work_cwd + photo_dir_list[i],
+                os.rename(work_cwd + '\\' + photo_dir_list[i],
                           os.path.abspath("已处理照片" + "\\" + num_of_tower) + '\\' + num_of_tower + new_name[
                               i] + ".jpg")
             print("转换完成。")
             showinfo('提示', '转换完成')
+            shutil.rmtree(work_cwd)
         else:
             print("不满足转换条件，未进行转换，请检查照片是否完整。")
             showwarning('警告', '不满足转换条件，未进行转换，请检查照片是否完整。')
